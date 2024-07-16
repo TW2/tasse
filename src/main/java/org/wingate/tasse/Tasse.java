a13f2cbd52e164ff039d3182a282334cee330af5
RedDog,csa <util2@msn.com>
2024-07-16 05:21:13 +0200

    init

---
+++ src/main/java/org/wingate/tasse/Tasse.java
@@ -0,0 +1,12 @@
+package org.wingate.tasse;
+
+/**
+ *
+ * @author util2
+ */
+public class Tasse {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
