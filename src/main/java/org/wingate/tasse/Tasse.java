d04694a8acb07e6e7431e5043739e7a8873a823e
RedDog,csa <util2@msn.com>
2024-07-16 05:40:41 +0200

    upload init 2

--- src/main/java/org/wingate/tasse/Tasse.java
+++ src/main/java/org/wingate/tasse/Tasse.java
@@ -1,5 +1,7 @@
 package org.wingate.tasse;
 
+import org.wingate.tasse.core.CreateFile;
+
 /**
  *
  * @author util2
@@ -7,6 +9,8 @@ package org.wingate.tasse;
 public class Tasse {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Format: java -jar tasse.jar asspath width height milliseconds");
+        System.out.println("Execute");
+        CreateFile.main(args);
     }
 }
